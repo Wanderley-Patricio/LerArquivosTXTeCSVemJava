dd95e15dcda333a48a6ddbdcfd54467b22dc0a4f
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-05-02 20:51:49 -0300

    1º Commit

---
+++ LerTXTeCSV/src/br/com/wpatricio/LerTXTeCSV.java
@@ -0,0 +1,18 @@
+/* Fontes
+ * https://github.com/devsuperior/aulao002
+ * https://www.youtube.com/watch?v=xLDViuYlqGM
+ * 
+ */
+package br.com.wpatricio;
+
+/**
+ *
+ * @author Adptado Wanderley Patrício
+ */
+public class LerTXTeCSV {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
