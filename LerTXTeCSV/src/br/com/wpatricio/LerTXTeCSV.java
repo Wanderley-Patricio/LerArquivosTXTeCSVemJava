a97017eadd87ee0110265b0634dd5891b69451b7
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-05-02 23:09:52 -0300

    3ª versão - Ler um arquivo texto utilizando o bloco try with resources

--- LerTXTeCSV/src/br/com/wpatricio/LerTXTeCSV.java
+++ LerTXTeCSV/src/br/com/wpatricio/LerTXTeCSV.java
@@ -3,15 +3,16 @@
  * https://www.youtube.com/watch?v=xLDViuYlqGM
  * 
  * Objetivo:
- * Ler um arquivo texto utilizando as classes FileReader e BufferedReader, com controle manual.
+ * Ler um arquivo texto utilizando o bloco 'try with resources'.
+ * o try with resources permite eu declarar o bloco try já instanciando os recursos na mesma linha do try.
+ * A vantagem é que tudo que eu instanciar de recursos nessa mesma linha do try vai ser desalocado de forma
+ * automática quando o escopo do try acabar
  */
 package br.com.wpatricio;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
-
 
 /**
  * @author Adptado por Wanderley Patrício de Sousa Neto
@@ -20,32 +21,20 @@ public class LerTXTeCSV {
 
     public static void main(String[] args) {
         String path = "C:\\0\\in.txt";
-        FileReader fr = null;   // 1º instancio o FileReader para termos uma referência de entrada do arquivo a ser lido. Depois,
-        BufferedReader br = null; // em cima do FileReader vou instanciar o BufferedReader
-                                // BufferedReader gerencia a memória usada para leitura, é uma classe mais otimizada e mais rápida.
-        try{
-            fr = new FileReader(path);   // 1º instancio o FileReader
-            br = new BufferedReader(fr); // 2º passo o FileReader como argumento para o BufferedReader. A partir da variável br eu 
-                                         // já tenho acesso a ficar lendo o arquivo.
-                                         // MACETE: Sempre que for ler um arquivo de texto leia linha a linha e faça o tratamento 
-                                         // nessa linha se for necessário.
+      
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){ // criei o bloco try já instanciando os recursos
+          //  fr = new FileReader(path);   // Eu instanciei o BufferedReader e passei a instância do FileReader já como parâmetro
+          //  br = new BufferedReader(fr); // para o bloco try
+                                         
             String line = br.readLine(); // para ler uma linha eu uso o método readLine
             while(line != null){         // se eu chegar no final do arquivo o método readline() me retorna nulo. 
                 System.out.println(line);
                 line = br.readLine();
             }
-        }
+        } // quando o programa chegar aqui ou acontecer uma exceção e cair no bloco catch, automaticamente os recursos instanciados pelo try serão desalocados.
+          // Outra vantagem desse método é que como ele já desaloca os recursos automaticamente eu não preciso do bloco finally
         catch(IOException e){
             System.out.println("Error: " + e.getMessage() );
-        }
-        finally{
-            try{
-                br.close(); // se fechar br e fr sem tratar pode gerar uma exceção
-                fr.close();
-            }
-            catch(IOException e){   // se chegar nesse ponto não tem muito o que se fazer pois está gerando um erro na hora de fechar o arquivo.
-               e.printStackTrace(); // Imprime na tela todo o StackTrace para análisarmos o que aconteceu. Já que não temos muito o que fazer para tratar.
-            }
         }
     }
 }
