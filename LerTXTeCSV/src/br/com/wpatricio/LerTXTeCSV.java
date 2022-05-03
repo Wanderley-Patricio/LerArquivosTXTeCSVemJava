97a332c5fce9fa8f62108f7da10e20d3d4ee58a6
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-05-02 22:43:31 -0300

    2ª versão - Ler um arquivo texto utilizando as classes FileReader e BufferedReader com controle manual.

--- LerTXTeCSV/src/br/com/wpatricio/LerTXTeCSV.java
+++ LerTXTeCSV/src/br/com/wpatricio/LerTXTeCSV.java
@@ -3,13 +3,15 @@
  * https://www.youtube.com/watch?v=xLDViuYlqGM
  * 
  * Objetivo:
- * Ler um arquivo texto utilizando a classe file e a classe scanner.
+ * Ler um arquivo texto utilizando as classes FileReader e BufferedReader, com controle manual.
  */
 package br.com.wpatricio;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
+
 
 /**
  * @author Adptado por Wanderley Patrício de Sousa Neto
@@ -17,32 +19,33 @@ import java.util.Scanner;
 public class LerTXTeCSV {
 
     public static void main(String[] args) {
-        File file = new File("C:\\0\\in.txt"); // instanciando um arquivo
-        Scanner scanner = null;                // Scannner pode ler dados da entrada padrão ou de um arquivo
-                                               // foi declarado fora do bloco de escopo try pois vou 
-                                               // fechar o scanner no final do processo para liberar recursos.
-        try {
-            scanner = new Scanner(file);       // instanciei o scanner
-            while (scanner.hasNextLine()) {    // hasNextLine retorna verdadeiro se houver outra linha
-                                               //Se scanner.hasNextLine ficar sublinhado de vermelho é 
-                                               //um erro indicando uma exceção FileNotFoundException
-//              String line = scanner.nextLine(); //ou declarando em uma linha só.
-//              System.out.println(line);
-                System.out.println(scanner.nextLine()); // se eu uso dessa forma não preciso criar variável String
+        String path = "C:\\0\\in.txt";
+        FileReader fr = null;   // 1º instancio o FileReader para termos uma referência de entrada do arquivo a ser lido. Depois,
+        BufferedReader br = null; // em cima do FileReader vou instanciar o BufferedReader
+                                // BufferedReader gerencia a memória usada para leitura, é uma classe mais otimizada e mais rápida.
+        try{
+            fr = new FileReader(path);   // 1º instancio o FileReader
+            br = new BufferedReader(fr); // 2º passo o FileReader como argumento para o BufferedReader. A partir da variável br eu 
+                                         // já tenho acesso a ficar lendo o arquivo.
+                                         // MACETE: Sempre que for ler um arquivo de texto leia linha a linha e faça o tratamento 
+                                         // nessa linha se for necessário.
+            String line = br.readLine(); // para ler uma linha eu uso o método readLine
+            while(line != null){         // se eu chegar no final do arquivo o método readline() me retorna nulo. 
+                System.out.println(line);
+                line = br.readLine();
             }
-            
-//            scanner.close(); // Se colocar nesse ponto do código e acontecer uma exceção o código pulará para o bloco catch
-                               // e o programa jamais fechará o scanner. Então atente-se para o correto local para fechar o scanner.
-            
-        } catch (IOException e) {                  // Pode-se colocar o FileNotFoundException ou se quiser ser mais genérico usar o
-                System.out.println(e.getMessage());// IOException, que é um tipo de exceção genérica que irá tratar todo tipo de exceção de                                          
-        }                                          // entrada e saída. Ex.: Arquivo não encotrado ou não tem permissão de leitura naquela pasta, etc.
-    
-        finally{ // finally é um bloco que vai executar mesmo se der certo ou errado (ele sempre é executado. Então para fechamento de recurso
-                 // convém colocar tudo dentro do bloco finally.
-            if(scanner != null){ // Outro ponto, se der uma exceção na hora de instânciar o scanner e o arquivo não for aberto eu não terei o que fechar.
-                scanner.close(); // Vai gerar uma exceção de ponteiro nulo ou NullPointerException. Para resolver o problema é garantir o fechamento uso o commando if
-            }                    
+        }
+        catch(IOException e){
+            System.out.println("Error: " + e.getMessage() );
+        }
+        finally{
+            try{
+                br.close(); // se fechar br e fr sem tratar pode gerar uma exceção
+                fr.close();
+            }
+            catch(IOException e){   // se chegar nesse ponto não tem muito o que se fazer pois está gerando um erro na hora de fechar o arquivo.
+               e.printStackTrace(); // Imprime na tela todo o StackTrace para análisarmos o que aconteceu. Já que não temos muito o que fazer para tratar.
+            }
         }
     }
 }
